07866b85be9ef124536d247b3c4615d3679e1a95
Jay Ding <jding@scentregroup.com>
2018-05-24 10:22:57 +1000

    Correct config file extension name

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,12 @@ FROM alpine:3.7 as builder
 RUN apk add --update curl && \
     mkdir -p /config && \
     curl -Ssl https://raw.githubusercontent.com/coreos/clair/master/config.yaml.sample > \
-      /config/config.yml
+      /config/config.yaml
 
 FROM alpine:3.7
 
 COPY docker-entrypoint.sh /
-COPY --from=builder /config/config.yml /config/config.yml
+COPY --from=builder /config/config.yaml /config/config.yaml
 
 VOLUME /config
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,9 +8,9 @@ echo '> reconfigure clair'
 : "${CLAIR_DB_SSLMODE:=disable}"
 : "${CLAIR_DB_UPDATE_INTERVAL:=24h}"
 
-sed -i "s|host=localhost|host=$CLAIR_DB_HOST|" /config/config.yml
-sed -i "s|user=postgres|user=$CLAIR_DB_USER password=$CLAIR_DB_PASSWORD|" /config/config.yml
-sed -i "s|sslmode=disable|sslmode=$CLAIR_DB_SSLMODE|" /config/config.yml
-sed -i "s|interval: 2h|interval: $CLAIR_DB_UPDATE_INTERVAL|" /config/config.yml
+sed -i "s|host=localhost|host=$CLAIR_DB_HOST|" /config/config.yaml
+sed -i "s|user=postgres|user=$CLAIR_DB_USER password=$CLAIR_DB_PASSWORD|" /config/config.yaml
+sed -i "s|sslmode=disable|sslmode=$CLAIR_DB_SSLMODE|" /config/config.yaml
+sed -i "s|interval: 2h|interval: $CLAIR_DB_UPDATE_INTERVAL|" /config/config.yaml
 
 echo "> $@" && exec "$@"
